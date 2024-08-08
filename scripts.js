fde37c8f935695507990c025abd60aec427291d2
Tim Perez <147675096+tim-perez@users.noreply.github.com>
2024-08-08 16:26:55 -0700

    Adding files

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementById("menu");
-const links = document.querySelector(".menu-links");
-
-function toggleMenu() {
-  links.classList.toggle("hidden");
-  menu.src = links.classList.contains("hidden") ? "images/menu.svg" : "images/close.svg";
-}
